71eb2cca49744b8f3d93d1dffd288f98504809ed
AlexKasema <lexxmulwa@gmail.com>
2023-11-17 19:47:27 +0300

    Implemented user login and logout functionality

--- base/views.py
+++ base/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.models import User, auth
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+
+from django.contrib.auth.decorators import login_required
 
 from . models import Profile
 
@@ -7,11 +10,12 @@ from django.contrib import messages #! for flash messages
 from django.http import HttpResponse
 
 # Create your views here.
-
+@login_required(login_url='signin')
 def index(request):
     return render(request, 'index.html')
 
 def signup(request):
+
 
     if request.method == 'POST':
         username = request.POST.get('username')
@@ -42,3 +46,25 @@ def signup(request):
             return redirect('signup')
     else:
         return render(request, 'signup.html')
+    
+def signin(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            messages.info(request, 'username or password is incorrect')
+            return redirect('signin')
+    else:
+        return render(request, 'signin.html')
+    
+@login_required(login_url='signin')
+def logoutUser(request):
+    logout(request)
+    return redirect('signin')
