1a7611a5b87a363b875b0b28e10d78a3445889ac
AlexKasema <lexxmulwa@gmail.com>
2023-11-19 14:03:10 +0300

    User suggestion Follow feature implementation

--- base/views.py
+++ base/views.py
@@ -10,6 +10,7 @@ from django.contrib import messages #! for flash messages
 from django.http import HttpResponse
 
 from itertools import chain
+import random
 
 # Create your views here.
 @login_required(login_url='signin')
@@ -31,9 +32,37 @@ def index(request):
     
     feed_list = list(chain(*feed)) #! now instead of sending posts to the view we can send feed_list
 
-    posts = Post.objects.all()
+    #! posts = Post.objects.all()  Don't need this since we have the feed_list
 
-    context = {'user_profile': user_profile, 'posts': feed_list}
+    #! User suggestions backend starts here
+
+    all_users = User.objects.all()
+    user_following_all = [] #! A list of all users the user is following
+
+    for user in user_following:
+        user_list = User.objects.get(username=user.user)
+        user_following_all.append(user_list)
+
+    #! filters all users we are not following and remove the logged in user from the list
+    new_suggestions_list = [x for x in list(all_users) if (x not in list(user_following_all))]
+    current_user = User.objects.filter(username=request.user.username)
+    final_suggestions_list = [x for x in list(new_suggestions_list) if (x not in list(current_user))]
+    random.shuffle(final_suggestions_list)
+
+    username_profile = []
+    username_profile_list = []
+
+    for users in final_suggestions_list:
+        username_profile.append(users.id)
+
+    for ids in username_profile:
+        profile_lists = Profile.objects.filter(id_user=ids)
+        username_profile_list.append(profile_lists)
+
+    suggestions_username_profile_list = list(chain(*username_profile_list))
+
+    context = {'user_profile': user_profile, 'posts': feed_list,
+               'suggestions_username_profile_list': suggestions_username_profile_list[:4]}
     return render(request, 'index.html', context)
 
 def signup(request):
@@ -45,7 +74,7 @@ def signup(request):
         password = request.POST.get('password')
         password2 = request.POST.get('password2')
         
-        if password == password2:
+        if password == password2 and password != None:
             if User.objects.filter(email=email).exists():
                 messages.info(request, 'Email already exists')
                 return redirect('signup')
